ab8db30f57e2e608b34129814f49ce60d0dc7eb3
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2025-12-03 14:25:10 -0300

    feat: add versionamento npm

--- .releaserc.js
+++ .releaserc.js
@@ -4,17 +4,21 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
-        "@semantic-release/github",
+        "@semantic-release/npm",
+        [
+            "@semantic-release/exec",
+            {
+                prepareCmd: "npm version ${nextRelease.version} --no-git-tag-version --prefix projects/ngx-opalbytes-directives",
+            },
+        ],
         [
             "@semantic-release/git",
             {
-                assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+                assets: ["CHANGELOG.md", "package.json", "package-lock.json", "projects/ngx-opalbytes-directives/package.json"],
                 message: "docs: ${nextRelease.version}",
             },
         ],
+        "@semantic-release/github",
     ],
-    prepareCmd: "npm version ${nextRelease.version} -m 'docs: %s'",
-    verifyConditions: ["@semantic-release/github"],
-    publish: ["@semantic-release/github"],
     repositoryUrl: "https://github.com/Micael-Macedo/opalbytes-directive-components",
 };
