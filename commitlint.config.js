725c0444dc61816ef042e6d3d6b558ffd5640a71
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2025-12-03 13:09:09 -0300

    chore: add commitlint and husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,39 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',     // Nova funcionalidade
-        'fix',      // Correção de bug
-        'docs',     // Documentação
-        'style',    // Formatação
-        'refactor', // Refatoração
-        'perf',     // Performance
-        'test',     // Testes
-        'build',    // Build/dependências
-        'ci',       // CI/CD
-        'chore',    // Outras alterações
-        'revert'    // Reverter commit
-      ]
-    ],
-    'scope-enum': [
-      2,
-      'always',
-      [
-        'directives',
-        'form',
-        'ui',
-        'structural',
-        'core',
-        'deps',
-        'config'
-      ]
-    ],
-    'subject-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
-    'header-max-length': [2, 'always', 100]
-  }
 };
